5416bf9ef9202d3df3c7ad134bc18cf7cbf25a3d
whitedokgu <npro107@gmail.com>
2022-06-14 20:52:11 +0900

    make monster search js

--- js/search.js
+++ js/search.js
@@ -1,18 +1,17 @@
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
+const searchInput = document.getElementById("search");
+const rows = document.querySelectorAll("tbody tr");
+//tbody의 tr이 선택되는지 확인
+//console.log(rows);
+searchInput.addEventListener("keyup", function (event) {
+  //input에 입력시 함수가 작동하는지 확인
+  //console.log(event);
+  const q = event.target.value;
+  rows.forEach((row) => {
+    //input에 입력시 tr값을 가져오는지 확인
+    console.log(row);
+    row.querySelector("td.td-name").textContent.toLowerCase().includes(q)
+      ? (row.style.display = "")
+      : (row.style.display = "none");
+    //console.log(q);
+  });
+});
